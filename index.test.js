2efa8596e7263e872990f60afb5e69ea4ffc0632
Amethyst Willia <zmithic@gmail.com>
2022-02-23 17:56:36 -0600

    Added and passed test 10

--- index.test.js
+++ index.test.js
@@ -82,7 +82,10 @@ describe('[Exercise 5] Seasons', () => {
   test('[9] the FIRST call of seasons.next returns "summer"', () => {
     expect(seasons.next()).toEqual('summer');
   });
-  // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
+  test('[10] the SECOND call of seasons.next returns "fall"', () => {
+    seasons.next();
+    expect(seasons.next()).toEqual('fall');
+  });
   // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
   // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
   // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
